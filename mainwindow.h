e5dac324c6bf1175deaca207340d8b7688fc0e56
MounibAdelll <sarray.mounib@esprit.tn>
2026-02-09 17:01:55 +0100

    inital commit

---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_label_3_linkActivated(const QString &link);
+
+
+
+    void on_verifyBtnForgot_clicked();
+
+    void on_cancelFogotBTN_clicked();
+
+    void on_CancelBTNCode_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
